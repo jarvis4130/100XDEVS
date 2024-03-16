12975b4726e996cbec1ac279b93957c0c5c4b0f5
jarvis4130 <atharvaadam101010@gmail.com>
2024-03-16 19:33:13 +0530

    solved some assignment

--- week-3/04-mongo-with-jwt-auth/routes/admin.js
+++ week-3/04-mongo-with-jwt-auth/routes/admin.js
@@ -1,22 +1,47 @@
 const { Router } = require("express");
 const adminMiddleware = require("../middleware/admin");
+const { Admin } = require("../db");
 const router = Router();
 
 // Admin Routes
-router.post('/signup', (req, res) => {
-    // Implement admin signup logic
+router.post("/signup", async (req, res) => {
+  // Implement admin signup logic
+  try {
+    if (!req.body.username || !req.body.password) {
+      res.json({
+        msg: "No username or password entered.",
+      });
+    }
+    await Admin.create({
+      username: req.body.username,
+      password: req.body.password,
+    });
+    res.json({
+      message: "Admin created successfully",
+    });
+  } catch (err) {
+    res.json({
+      msg: "Error",
+    });
+  }
 });
 
-router.post('/signin', (req, res) => {
-    // Implement admin signup logic
+router.post("/signin", (req, res) => {
+  // Implement admin signup logic
+//   Description: Logs in an admin account.
+//   Input Body: { username: 'admin', password: 'pass' }
+//   Output: { token: 'your-token' }
+
+    
+
 });
 
-router.post('/courses', adminMiddleware, (req, res) => {
-    // Implement course creation logic
+router.post("/courses", adminMiddleware, (req, res) => {
+  // Implement course creation logic
 });
 
-router.get('/courses', adminMiddleware, (req, res) => {
-    // Implement fetching all courses logic
+router.get("/courses", adminMiddleware, (req, res) => {
+  // Implement fetching all courses logic
 });
 
 module.exports = router;
