533f2216b04b88919dc7d2eda8c76c0634d77029
jarvis4130 <atharvaadam101010@gmail.com>
2024-03-24 11:17:09 +0530

    TypeScript Done

---
+++ week-9/9.2/node-app/a.js
@@ -0,0 +1,9 @@
+"use strict";
+// ts variable assign
+const x = 1;
+console.log(x);
+// ts function with args defined
+function greet(firstName) {
+    console.log("Hello " + firstName);
+}
+greet("Atharva Adam");
---
+++ week-9/9.2/node-app/a.ts
@@ -0,0 +1,9 @@
+// ts variable assign
+const x:number=1
+console.log(x)
+
+// ts function with args defined
+function greet(firstName:string){
+    console.log("Hello "+firstName)
+}
+greet("Atharva Adam")
